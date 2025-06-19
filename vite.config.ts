b3dfe7cd69ab1171bf90cf9c430972f790501a8a
Amandasfs <amandsfsantos@gmail.com>
2025-06-19 20:32:51 -0300

    Name Change

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WeddingPage01/',   // <- ESSENCIAL PARA O GITHUB PAGES FUNCIONAR
-});
+  base: '/WeddingPage01/',   // Essencial para GitHub Pages
+})
